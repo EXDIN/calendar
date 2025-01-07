c5c6f9c1b9b55ab54b44b35f837e75573a03587d
EXDIN <exdin1@gmail.com>
2025-01-07 23:41:39 +0200

    add date thx

---
+++ src/constants/date.ts
@@ -0,0 +1,24 @@
+export const weekDays = [
+    "Sun",
+    "Mon",
+    "Tue",
+    "Wen",
+    "Thu",
+    "Fri",
+    "Sat",
+]
+
+export const months = [
+    "January",
+    "Fabruary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
