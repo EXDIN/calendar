d6ffbd5bf02fd51f098dec74d4dee618cd665106
EXDIN <exdin1@gmail.com>
2025-01-08 19:39:21 +0200

    update page404

---
+++ src/pages/Page404/Page404.styles.ts
@@ -0,0 +1,13 @@
+import { styled } from "@stitches/react";
+
+export const MyDiv = styled("span", {
+    padding: '15px 25px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: '15px',
+    backgroundColor: 'black',
+    fontSize: '20px',
+    fontWeight: '600',
+    color: 'white'
+})
--- src/pages/Page404/Page404.tsx
+++ src/pages/Page404/Page404.tsx
@@ -1,26 +1,14 @@
 import { Link } from "react-router-dom";
 import Pages from "../../routing/pages-enum";
-import { styled } from "@stitches/react";
-
-const MyDiv = styled("span", {
-  padding: '15px 25px',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center', 
-  borderRadius: '15px',
-  backgroundColor: 'black',
-  fontSize: '20px',
-  fontWeight: '600',
-  color: 'white'
-})
+import { MyDiv } from "./Page404.styles";
 
 
 function Page404() {
-  return (
-      <Link to={Pages.Calendar}>
-          <MyDiv>This page wasn`t found, back to Calendar</MyDiv>
-      </Link>
-  )
+    return (
+        <Link to={Pages.Calendar}>
+            <MyDiv>This page wasn`t found, back to Calendar</MyDiv>
+        </Link>
+    )
 }
 
 export default Page404;
