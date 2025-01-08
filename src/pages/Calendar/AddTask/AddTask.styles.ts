73eb62d8bf16011fc7bf5e1b84af76cc4edfb997
EXDIN <exdin1@gmail.com>
2025-01-08 19:51:09 +0200

    addtask component

---
+++ src/pages/Calendar/AddTask/AddTask.styles.ts
@@ -0,0 +1,54 @@
+import { styled } from "@stitches/react";
+
+export const OpenButton = styled("button", {
+    backgroundColor: 'beige',
+    fontSize: '20px',
+    padding: '10px 20px',
+    borderRadius: '10px',
+    cursor: 'pointer',
+    border: 'none',
+});
+
+export const AddForm = styled("form", {
+    width: '100%',
+    height: '100%',
+    padding: '0',
+    textAlign: 'center',
+});
+
+export const DateInput = styled("input", {
+    width: '50%',
+    height: '10%',
+    backgroundColor: 'beige',
+    borderRadius: '20px',
+    padding: '5px 15px',
+    border: '5px solid #1c1e26',
+    fontSize: '50px',
+    color: '#1c1e26',
+    marginBottom: '20px',
+    textAlign: 'center',
+});
+
+export const DescInput = styled("input", {
+    width: '95%',
+    height: '65%',
+    backgroundColor: 'beige',
+    fontSize: '50px',
+    color: '#1c1e26',
+    resize: 'none',
+    borderRadius: '20px',
+    padding: '5px 15px',
+    textAlign: 'start',
+});
+
+export const SubmBtn = styled("button", {
+    marginTop: '25px',
+    marginBottom: '25px',
+    width: '30%',
+    height: '10%',
+    backgroundColor: 'beige',
+    borderRadius: '20px',
+    fontSize: '50px',
+    color: '#1c1e26',
+    cursor: 'pointer',
+});
---
+++ src/pages/Calendar/AddTask/AddTask.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react"
+import { Task } from "../../../store/tasks/tasks.slice"
+import ModalComponent from "../../../components/ModalComponent/ModalComponent"
+import { useDispatch } from "react-redux"
+import { addTask } from '../../../store/tasks/tasks.slice';
+import { yesterday } from "../../../helpers/day"
+import { AddForm, DateInput, DescInput, OpenButton, SubmBtn } from "./AddTask.styles"
+
+
+
+function AddTask() {
+    const [modalActive, setModalActive] = useState(false);
+    const [data, setData] = useState<Task>({} as Task);
+    const dispatch = useDispatch();
+
+    const handleAddTask = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const date = yesterday(data.date);
+
+        if (data.date && data.description) {
+            dispatch(addTask({
+                id: Date.now().toString(),
+                date: date,
+                description: data.description,
+            }));
+        }
+
+        setModalActive(false);
+    };
+
+    return (
+        <>
+            <OpenButton onClick={() => setModalActive(true)}>Add new Task</OpenButton>
+            {modalActive
+                ?
+                <ModalComponent active={modalActive} setActive={setModalActive}>
+                    <AddForm onSubmit={e => handleAddTask(e)}>
+                        <DateInput type="date" onChange={(e) => setData({ ...data, date: e.currentTarget.value })}></DateInput>
+                        <DescInput type="text" onChange={(e) => setData({ ...data, description: e.currentTarget.value })} placeholder="Description"></DescInput>
+                        <SubmBtn type="submit">Create Task</SubmBtn>
+                    </AddForm>
+                </ModalComponent>
+                : null}
+        </>
+    )
+}
+
+export default AddTask;
